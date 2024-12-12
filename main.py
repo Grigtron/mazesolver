15907cf53bc14de1f7b68ce01347eb0313591f94
Grigtron <jgregas@gmail.com>
2024-12-11 19:54:13 -0500

    Added Cell drawing

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from window import *
+from cell import *
 
 
 def main():
     win = Window(800,600)
-    l = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(l, "black")
+
+    c = Cell(win)
+    c.has_left_wall = True
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = True
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = True
+    c.draw (225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = True
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 
