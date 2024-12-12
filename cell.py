74a8f879334fbd7493cfa024d56a1f3a0c97bd4d
Grigtron <jgregas@gmail.com>
2024-12-12 18:53:20 -0500

    Added Cell to Cell line drawing and fixed a couple typos in earlier Cell code

--- cell.py
+++ cell.py
@@ -1,4 +1,4 @@
-from window import Line, Point
+from window import *
 
 class Cell:
     def __init__(self, win):
@@ -20,14 +20,22 @@ class Cell:
         self._y1 = y1
         self._y2 = y2
         if self.has_left_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
-            self._win.draw_line(line)
-        if self.has_bottom_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
-            self._win.draw_line(line)
-        if self.has_right_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
+            line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
         if self.has_top_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
+            line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+
+    def get_center(self):
+        return Point(self._x1 + (abs(self._x2 - self._x1) // 2), self._y1 + (abs(self._y2 - self._y1) // 2))
+
+    def draw_move(self, to_cell, undo=False):
+        move_line = Line(self.get_center(), to_cell.get_center())
+        self._win.draw_line(move_line, "gray" if undo else "red")
+        
--- main.py
+++ main.py
@@ -6,20 +6,14 @@ def main():
     win = Window(800,600)
 
     c = Cell(win)
-    c.has_left_wall = True
+    c.has_bottom_wall = False
     c.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_right_wall = True
-    c.draw(125, 125, 200, 200)
+    d = Cell(win)
+    d.has_top_wall = False
+    d.draw(50, 100, 100, 150)
 
-    c = Cell(win)
-    c.has_bottom_wall = True
-    c.draw (225, 225, 250, 250)
-
-    c = Cell(win)
-    c.has_top_wall = True
-    c.draw(300, 300, 500, 500)
+    Cell.draw_move(c, d, undo=True)
 
     win.wait_for_close()
 
